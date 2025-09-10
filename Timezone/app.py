9d085cbf9d03285364e5f9b836d00b51450f5836
Ahmad Hassan <ahmadabuulhassan@gmail.com>
2025-09-10 16:30:36 +0500

    Update app.py

--- Timezone/app.py
+++ Timezone/app.py
@@ -577,3 +577,5 @@ with app.app_context():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Railway gives PORT, fallback=5000 for local
+    app.run(host="0.0.0.0", port=port)
